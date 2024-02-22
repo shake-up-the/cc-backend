3ce28e1bce32ba1d146645b8f717c4c0f51ba788
kim2ju <bamtori05@naver.com>
2024-02-22 11:43:03 +0900

    feat: exception advice 추가

---
+++ src/main/java/com/cc/exception/ExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.cc.exception;
+
+import com.cc.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response exception(Exception e) {
+        log.info("e = {}", e.getMessage());
+        return Response.failure(-1000, "오류가 발생하였습니다.");
+    }
+}
